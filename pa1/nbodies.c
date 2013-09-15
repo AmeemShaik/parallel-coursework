1ffa6454c8aedb118695f46c9940d8b304671f13
Zach Cross <zcross@cs.unc.edu>
2013-09-15 16:11:04 -0400

    Used initial values as specified in assignment... parameterized timestep

--- pa1/nbodies.c
+++ pa1/nbodies.c
@@ -10,13 +10,16 @@
 
 #define G 1.0
 #define X_MIN 0.0
-#define X_MAX 1024.0
+#define X_MAX 1.0
 #define Y_MIN 0.0
-#define Y_MAX 1024.0
-#define MASS_MIN 100
-#define MASS_MAX 1000
+#define Y_MAX 1.0
+#define MASS_MIN 0.125
+#define MASS_MAX 0.975
+#define INIT_V_MIN -1.0
+#define INIT_V_MAX 1.0
 
 int n, k;
+double timestep;
 
 struct body
 {
@@ -101,34 +104,49 @@ void printState(int i) {
 
 int main(int argc, char **argv) {
 
-    if (argc != 3) {
-        printf("Expected 2 arguments: nBodies kSteps.\n");
+    if (argc != 4) {
+        printf("Expected 2 arguments: numBodies timeStep numSteps.\n");
         return EXIT_FAILURE;
     }
 
     n = atoi(argv[1]);
-    k = atoi(argv[2]);
+    timestep = strtod(argv[2], NULL);
+    k = atoi(argv[3]);
 
-    if (n <= 0 || k <= 0) {
-        printf("Expected non-zero values for n and k.\n");
+    printf("Received parameters:\n\t# Bodies: %d\n\tTime Step: %f\n\t# Steps:%d\n",
+           n, timestep, k);
+
+    if (n <= 0 || k <= 0 || timestep <= 0) {
+        printf("Expected non-zero values for n, ∆t, and k.\n");
         return EXIT_FAILURE;
     }
 
     b = (struct body *) malloc( n * sizeof(struct body));
 
     //Todo: parameterize timestep
-    int t, timeStep = 1;
+    int t;
 
     // Initialize bodies
     int j;
     for(j=0; j < n; j++) {
+
+        double vx = (double)rand() * (INIT_V_MAX - INIT_V_MIN) / (double)RAND_MAX + INIT_V_MIN;
+        double vy = (double)rand() * (INIT_V_MAX - INIT_V_MIN) / (double)RAND_MAX + INIT_V_MIN;
+        
+        // Ensure |v| < 1
+        double vmagSq = vx*vx + vy*vy;
+        if( vmagSq >= 1 ) {
+            vx = vx/vmagSq - 0.00125;
+            vy = vy/vmagSq - 0.00125;
+        }
+
         init(
             j,
             (double)rand() * (MASS_MAX - MASS_MIN) / (double)RAND_MAX + MASS_MIN,   
             (double)rand() * (X_MAX - X_MIN) / (double)RAND_MAX + X_MIN,
             (double)rand() * (Y_MAX - Y_MIN) / (double)RAND_MAX + Y_MIN,
-            0.0,
-            0.0
+            vx,
+            vy
         );
         printState(j);
     }
@@ -145,11 +163,13 @@ int main(int argc, char **argv) {
         }
 
         for(i = 0; i < n ; i++){
-            b[i].r_x += timeStep * b[i].v_x;
-            b[i].r_y += timeStep * b[i].v_y;
-            b[i].v_x += timeStep * b[i].a_x;
-            b[i].v_y += timeStep * b[i].a_y;
+            b[i].r_x += timestep * b[i].v_x;
+            b[i].r_y += timestep * b[i].v_y;
+            b[i].v_x += timestep * b[i].a_x;
+            b[i].v_y += timestep * b[i].a_y;
+            #ifdef VERBOSE
             printState(i);
+            #endif
         }
     }
 
