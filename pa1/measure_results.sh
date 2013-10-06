e6926510c5f7d51310c2e320823ac7eadcef5afa
Zach Cross <zcross@cs.unc.edu>
2013-10-06 09:32:12 -0400

    Wow, really dumb idea on my part. Build up output incrementally

--- pa1/measure_results.sh
+++ pa1/measure_results.sh
@@ -7,8 +7,14 @@ STEPS=${2:-$DEFAULT_STEPS}
 P_VALUES=(1 2 4 8 16)
 N_VALUES=(100 200 500 1000 2000 5000 10000)
 
-echo "Running simulations with varying (n, p, k):"
-echo "  Time step size t=$TIMESTEP"
+echo "Compiling programs"
+make
+
+rm -f results.csv
+touch results.csv
+
+echo "Running simulations with varying (n, p, k):" >> results.csv
+echo "  Time step size t=$TIMESTEP" >> results.csv
 
 echo "n, p, k, newtons, walltime"
 for p in ${P_VALUES[*]}
@@ -16,8 +22,8 @@ do
     for n in ${N_VALUES[*]}
     do
         export OMP_NUM_THREADS=$p
-        ./nbodies $n $TIMESTEP $STEPS
-        ./nbodies_newton $n $TIMESTEP $STEPS
+        ./nbodies $n $TIMESTEP $STEPS >> results.csv
+        ./nbodies_newton $n $TIMESTEP $STEPS >> results.csv
         #each will print (n, p, k, newtons{0,1}, walltime)
     done
 done
