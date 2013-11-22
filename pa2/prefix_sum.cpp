17c717ff3dc2460d2bfb0485dbc05281ce462145
Zach Cross <zcross@cs.unc.edu>
2013-11-22 14:48:09 -0500

    segfault hooray

--- pa2/prefix_sum.cpp
+++ pa2/prefix_sum.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
 
@@ -6,6 +7,8 @@
     an output S[1..n], a vector of prefix sums.
 */
 void parallel_prefix_sum(long *X, long *S, int n, int k) {
+
+    assert (n == (1 << k));
 
     cilk_for(int i = 0; i < n; i++) {
         S[i] = X[i];
@@ -27,8 +30,33 @@ void parallel_prefix_sum(long *X, long *S, int n, int k) {
 
 }
 
+void printArray(long *A, int n){
+    using namespace std;
+
+    cout << "[";
+    for (int i = 0; i < n ; i++) {
+        cout << A[i];
+        if ( i != n-1) {
+            cout << ", ";
+        }
+    }
+    cout << "]\n";
+}
+
 int main(int argc, char **argv) {
 
+    // Test parallel prefix sum.
+    // n = 8; k = 3
+    long mynumbers[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    long mynumbers_pfx[8];
+
+    printf("Before sum, mynumbers=\n");
+    printArray(mynumbers, 8);
+    parallel_prefix_sum(mynumbers, mynumbers_pfx, 8, 3);
+    printf("After sum, mynumbers=\n");
+    printArray(mynumbers, 8);
+    printf("And the prefix sum array=\n");
+    printArray(mynumbers_pfx, 8);
 
 }
 
