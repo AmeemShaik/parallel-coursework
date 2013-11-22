99e02d554970458b9589f4ba1966337a3b66900c
Zach Cross <zcross@cs.unc.edu>
2013-11-22 15:57:30 -0500

    well, it compiles

--- pa2/prefix_sum.c
+++ pa2/prefix_sum.c
@@ -1,4 +1,6 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
 
@@ -20,7 +22,7 @@ void printArray(long *A, int n){
     Takes an input X[1..n], n=2^k and produces
     an output S[1..n], a vector of prefix sums.
 */
-void parallel_prefix_sum(long *X, long *S, int n, int k) {
+void parallel_prefix_sum(short int *X, int *S, int n, int k) {
 
     assert (n == (1 << k));
     assert (X != NULL && S != NULL);
@@ -31,20 +33,12 @@ void parallel_prefix_sum(long *X, long *S, int n, int k) {
         S[i - 1] = X[i - 1];
     }
 
-    printf("Just initialized S\n");
-    printArray(S, n);
-
     for(h = 1 ; h <= k ; h++) {
         cilk_for (i = 1; i <= ( n >> h) ; i++) {
-            printf("i*2^h = %d\n", i*(1 << h));
-            printf("(2^(h-1) = %d\n", (1 << (h-1)));
             // S[i * (1 << h)] = S[i * (1 << h) - (1 << (h-1))] + S[i * (i << h)];
             S[i * (1 << h) - 1] += S[(1 << h) * i - (1 << (h-1)) - 1];
         }
     }
-
-    printf("First loop done\n");
-    printArray(S, n);
 
     for (h = k ; h >= 1; h--) {
         cilk_for(i = 2; i <= (n >> (h-1)) ; i++){
@@ -56,21 +50,97 @@ void parallel_prefix_sum(long *X, long *S, int n, int k) {
 
 }
 
+void quicksort(long *array,int left,int right){
+    //select the first element as pivot
+    if(left<right){
+        int splitPoint = partition(array,left, right);
+        cilk_spawn quicksort(array,left,splitPoint-1);
+        quicksort(array,splitPoint+1,right);
+    }
+}
+int partition(long *array, int left, int right){
+    
+    int n = (right - left + 1);
+    int k = (int) log(n);
+    long result[n];
+
+    // Flag arrays for less than, greater than, equal to.
+    short int lt[n],
+              eq[n],
+              gt[n];
+
+    int lt_indices[n],
+        eq_indices[n],
+        gt_indices[n];
+
+    int i;
+    cilk_for (i = 0; i < n; i++) {
+        if (array[i] < array[right]) {
+            lt[i] = 1;
+            eq[i] = 0;
+            gt[i] = 0;
+        } else if (array[i] == array[right]) {
+            lt[i] = 0;
+            eq[i] = 1;
+            gt[i] = 0;
+        } else {
+            lt[i] = 0;
+            eq[i] = 0;
+            gt[i] = 0;
+        }
+    }
+
+    parallel_prefix_sum(lt, lt_indices, n, k);
+    parallel_prefix_sum(eq, eq_indices, n, k);
+    parallel_prefix_sum(gt, gt_indices, n, k);
+
+    int lt_index_max = lt_indices[n-1],
+        eq_index_max = eq_indices[n-1] + lt_index_max;
+
+    cilk_for (i = 0; i < n; i++) {
+        if (lt[i]) {
+            result[lt_indices[i] - 1] = array[i];
+        } else if (eq[i]) {
+            result[eq_indices[i] + lt_index_max - 1] = array[i];
+        } else if (gt[i]) {
+            result[gt_indices[i] + eq_index_max - 1] = array[i];
+        }
+    }
+
+    cilk_for (i = 0; i < n; i++) {
+        array[i] = result[i];
+    }
+
+}
+
 int main(int argc, char **argv) {
 
-    // Test parallel prefix sum.
-    // n = 8; k = 3
-    long mynumbers[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    long mynumbers_pfx[8];
+    if(argc!=2){
+            printf("Incorrect number of arguments, expected 1 argument\n");
+            return EXIT_FAILURE;
+        }
+        int size = atoi(argv[1]);
+        long *array;
+        array = malloc(size*sizeof(long));
+        int i;
+        srand(time(NULL));
+        for(i = 0; i < size; i++){
+            long r = rand()%size;
+            array[i] = r;
+        }
+        #ifdef PRINTMODE
+        printf("Unsorted Array\n");
+        printArray(array,size);
+        #endif
 
-    printf("Before sum, mynumbers=\n");
-    printArray(mynumbers, 8);
-    parallel_prefix_sum(mynumbers, mynumbers_pfx, 8, 3);
-    printf("After sum, mynumbers=\n");
-    printArray(mynumbers, 8);
-    printf("And the prefix sum array=\n");
-    printArray(mynumbers_pfx, 8);
-
+        int left = 0;
+        int right = size-1;
+        quicksort(array, left, right);
+        #ifdef PRINTMODE
+        printf("Sorted Array\n");
+        printArray(array,size);
+        #endif
+        return 0;
 }
 
 
--- pa2/quicksort_cilk.c
+++ pa2/quicksort_cilk.c
@@ -7,6 +7,46 @@ void quicksort(long long*,int,int);
 int partition(long long*,int,int);
 void printArray(long long*, int size);
 
+/* Inclusive, in-place parallel prefix sum. 
+    Takes an input X[1..n], n=2^k and produces
+    an output S[1..n], a vector of prefix sums.
+*/
+void parallel_prefix_sum(long *X, long *S, int n, int k) {
+
+    assert (n == (1 << k));
+    assert (X != NULL && S != NULL);
+
+    int i, h;
+
+    cilk_for(i = 1; i <= n; i++) {
+        S[i - 1] = X[i - 1];
+    }
+
+    printf("Just initialized S\n");
+    printArray(S, n);
+
+    for(h = 1 ; h <= k ; h++) {
+        cilk_for (i = 1; i <= ( n >> h) ; i++) {
+            printf("i*2^h = %d\n", i*(1 << h));
+            printf("(2^(h-1) = %d\n", (1 << (h-1)));
+            // S[i * (1 << h)] = S[i * (1 << h) - (1 << (h-1))] + S[i * (i << h)];
+            S[i * (1 << h) - 1] += S[(1 << h) * i - (1 << (h-1)) - 1];
+        }
+    }
+
+    printf("First loop done\n");
+    printArray(S, n);
+
+    for (h = k ; h >= 1; h--) {
+        cilk_for(i = 2; i <= (n >> (h-1)) ; i++){
+            if (i % 2) {
+                S[i * (1 << (h-1)) -1] = S[i * (1 << (h-1)) - (1 << (h-1)) - 1] + S[i * (1 << (h-1)) -1];
+            }
+        }
+    }
+
+}
+
 static unsigned int size;
 int main(int argc, char **argv)
 {
